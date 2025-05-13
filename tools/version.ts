914f2ce6aa01523e43b917f61d21a011037c990e
Sergey Kulikov <ksa@raa-st.com>
2025-05-13 13:37:24 +0300

    First commit

---
+++ tools/version.ts
@@ -0,0 +1,28 @@
+import { $ } from "bun";
+// Use Bun when Bun supports node:fs/promises fully
+import { writeFile } from "node:fs/promises";
+
+// Version package
+await $`bunx changeset version`;
+
+// Import versioned package.json
+const {
+	default: { version },
+} = await import("../package.json", {
+	with: { type: "json" },
+});
+
+// Import system.json
+const { default: manifest } = await import("../system.json", {
+	with: { type: "json" },
+});
+
+// Update and Write system.json
+manifest.version = version;
+manifest.download = manifest.download.replace(/v\d+\.\d+\.\d+/, `v${version}`);
+await writeFile("system.json", `${JSON.stringify(manifest, null, "\t")}\n`);
+
+// Format system.json
+await $`bunx biome format --write .`;
+await $`git add -A`;
+await $`git commit -m "chore: Version Packages"`;
